7f510e49335a766d5df82ad018f4d420495a0b87
Dennis Duka <dukadennis7@gmail.com>
2022-10-23 17:57:08 +0100

    added ProjectListItems

---
+++ components/project_link_item.tsx
@@ -0,0 +1,69 @@
+import { Box, keyframes, Link, ListItem, SystemStyleObject, useColorModeValue } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import { MouseEvent, TransitionEvent } from 'react';
+import theme from '../lib/theme';
+
+const ProjectListItem = ({ href, children, emoji }) => {
+
+    const listItemStyle = {
+        display: 'flex',
+        alignItems: 'center'
+    }
+
+    const rotationKeyframe = keyframes`
+        0% {transform: rotate(0deg);}
+        15% {transform: rotate(-20deg);}
+        50% {transform: rotate(10deg);}
+        85% {transform: rotate(0deg);}
+        100% {transform: rotate(0deg);}
+    `
+
+    const emojiStyle: SystemStyleObject = {
+        display: 'none',
+        margin: '0 3px',
+        animation: `${rotationKeyframe} 1s`,
+        color: useColorModeValue(theme.colors.steelBlue, theme.colors.yellowCrayola)
+    }
+
+    const titleAfterStyle: SystemStyleObject = {
+        textDecoration: "none",
+        backgroundImage: "linear-gradient(currentColor, currentColor)",
+        backgroundPosition: "0% 100%",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "0% 2px",
+        transition: "background-size 0.3s",
+        _hover: {
+            backgroundSize: "100% 2px"
+        },
+        _focus: {
+            backgroundSize: "100% 2px"
+        }
+    }
+
+    const showEmoji = (event: TransitionEvent<HTMLElement> | MouseEvent<HTMLElement>) => {
+        const element = event.currentTarget;
+        const emoji = element.parentElement.lastElementChild as HTMLElement;
+        if (element.matches(':hover')) {
+            emoji.style.display = "inline-block"
+        }
+        else {
+            emoji.style.display = "none"
+        }
+    }
+
+    return (
+        <ListItem sx={listItemStyle}>
+            <Box style={{ display: 'inline-flex', alignItems: 'center' }}>
+                <NextLink href={href} passHref>
+                    <Box style={{ display: 'inline-flex', alignItems: 'center' }}>
+                        <Link sx={titleAfterStyle} onTransitionEnd={showEmoji} onMouseLeave={showEmoji} className="projectLink">{children}</Link>
+                        <Box sx={emojiStyle} animation={rotationKeyframe}>{emoji}</Box>
+                    </Box>
+                </NextLink>
+            </Box>
+        </ListItem>
+    )
+}
+
+
+export default ProjectListItem
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
-import { Container, Box, Heading} from "@chakra-ui/react"
+import { Container, Box, Heading, List} from "@chakra-ui/react"
+import Paragraph from "../components/paragraph"
 import Section from '../components/section'
 import SectionTitle from "../components/section_title"
+import { BsFileEarmarkLockFill } from 'react-icons/bs'
+import { FaGasPump } from 'react-icons/fa'
+import { TbAtom } from 'react-icons/tb'
+import ProjectListItem from '../components/project_link_item'
 
 const Page = () => {
     return (
@@ -19,19 +24,25 @@ const Page = () => {
                 </Heading>
                 <Box>
                     <SectionTitle title="M.Sc. Computing (Software Engineering)" institution="Imperial College London" image="../images/imperial.png" />
-                    {/* <Paragraph>
+                    <Paragraph>
                         This one was challenging... but also quite fun! Chose security modules plus machine learning, robitics and distributed ledgers
-                    </Paragraph> */}
-                    {/* <List fontWeight={40} >
-                        <ListItem display='flex' alignItems={'center'}><BsFileEarmarkLockFill/>Mitigating Encryption from Ransomware</ListItem>
-                        <ListItem display='flex' alignItems={'center'}><FaGasPump/> FuelOpt</ListItem>
-                    </List> */}
+                    </Paragraph>
+                    <List fontWeight={40} >
+                        <ProjectListItem href="https://github.com/GreyTeddy/ransomware_mitigation" emoji={<BsFileEarmarkLockFill />}>
+                            Mitigating Encryption from Ransomware
+                        </ProjectListItem>
+                        <ProjectListItem href="" emoji={<FaGasPump />}>
+                            FuelOpt
+                        </ProjectListItem>
+                    </List>
                 </Box>
                 <Box>
                     <SectionTitle title="B.Sc. Computer Science" institution="Goldsmiths, University of London" image="../images/goldsmiths.png" />
-                    {/* <List fontWeight={40} >
-                        <ListItem display='flex' alignItems={'center'}> <TbAtom/> Quantum Computing, Cryptography and the Hidden Subgroup Problem</ListItem>
-                    </List> */}
+                    <List fontWeight={40} >
+                        <ProjectListItem href="" emoji={<TbAtom />}>
+                            Quantum Computing, Cryptography and the Hidden Subgroup Problem
+                        </ProjectListItem>
+                    </List>
                 </Box>
             </Section>
         </Container>
