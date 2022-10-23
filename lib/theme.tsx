6a82758019982473f58406eb6a503a9b62c5c4f0
Dennis Duka <38925592+GreyTeddy@users.noreply.github.com>
2022-10-23 16:36:23 +0000

    change default colour for links and drawerbody

--- lib/theme.tsx
+++ lib/theme.tsx
@@ -35,12 +35,12 @@ const components = {
     },
     Link: {
         baseStyle: props => ({
-            color: mode(colors.jet, colors.celadon)(props),
+            color: mode(colors.jet, colors.yellowCrayola)(props),
         })
     },
     DrawerBody: {
         baseStyle: props => ({
-            background: mode(colors.jet, colors.celadon)(props),
+            background: mode(colors.jet, colors.yellowCrayola)(props),
         })
     }
 }
