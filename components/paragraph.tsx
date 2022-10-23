a0b21c93f104c2b8e069950081607eb00f23ed32
Dennis Duka <dukadennis7@gmail.com>
2022-10-24 00:21:14 +0100

    changed font weight for paragraph

--- components/paragraph.tsx
+++ components/paragraph.tsx
@@ -4,7 +4,7 @@ import { Box, Text } from '@chakra-ui/react';
 const Paragraph = ({children}) => {
     return (
         <Box>
-            <Text style={{textAlign: "justify",textIndent: "1em", fontWeight:"100"}}>
+            <Text style={{textIndent: "1em", fontWeight:"200",alignContent:'center'}}>
                 {children}
             </Text>
         </Box>
