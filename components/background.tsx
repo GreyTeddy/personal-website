34d5d78b04a58460e86d0741d9902c1043c1755a
Dennis Duka <dukadennis7@gmail.com>
2022-10-25 11:54:46 +0100

    changed background image

--- components/background.tsx
+++ components/background.tsx
@@ -2,24 +2,18 @@ import { Box, keyframes, useColorModeValue } from "@chakra-ui/react"
 
 const Background = () => {
 
-    const darkImage = 'url("../images/teddy_400_dark.png")';
-    const lightImage = 'url("../images/teddy_400_light.png")';
+    const darkImage = 'url("../images/teddy_icon_dark.png")';
+    const lightImage = 'url("../images/teddy_icon_light.png")';
 
     const rotationKeyframe = keyframes`
       0% {
-        transform: translate3d(0, 0, 0);
-      }
-      25% {
-        transform: translate3d(0, 5px, 0);
-      }
-      50% {
-        transform: translate3d(0, 0, 0);
-      }
-      75% {
         transform: translate3d(0,-5px, 0);
       }
+      50% {
+        transform: translate3d(0, 5px, 0);
+      }
       100% {
-        transform: translate3d(0, 0, 0);
+        transform: translate3d(0,-5px, 0);
       }
     `
 
@@ -29,10 +23,10 @@ const Background = () => {
             position: 'fixed',
             // bottom: 0,
             left: "50%",
-            height: "450px",
-            width: "450px",
+            height: "550px",
+            width: "730px",
             background: useColorModeValue(lightImage, darkImage),
-            animation: `${rotationKeyframe} 5s linear infinite`,
+            animation: `${rotationKeyframe} 10s linear infinite`,
             // backgroundRepeat: 'repeat',
             // filter: 'invert(1)'
         }}></Box>
