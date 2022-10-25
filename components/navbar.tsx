fa514c598e1fb340a2e62ea89e8ba48e2193beac
Dennis Duka <dukadennis7@gmail.com>
2022-10-25 13:12:49 +0100

    better positioned and stylised navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,19 +5,25 @@ import {
     Box,
     Heading,
     Flex,
+    SystemStyleObject,
+    useColorModeValue,
 } from '@chakra-ui/react'
 
 const NavBar = () => {
+    const navbarStyle: SystemStyleObject = {
+        backgroundColor: useColorModeValue('rgba(220,220,220,0.4)','rgba(50,50,50,0.4)'),
+        borderBottomRadius: '15'
+    }
+
     return (
         <Box
             position="fixed"
             as="nav"
             w="100%"
-            style={{ backdropFilter: 'blur(1px)' }}
-            z-index={1}>
-            <Container width="100%" display='flex' p={2} maxW='container.md' justifyContent={'center'}>
+            zIndex={10}>
+            <Container width="100%" display='flex'  maxW='container.md' justifyContent={'center'}>
                 <Flex >
-                    <Heading size='lg' letterSpacing={'tight'}>
+                    <Heading size='lg' letterSpacing={'tight'} p={0.5} sx={navbarStyle}>
                         <Logo />
                     </Heading>
                 </Flex>
