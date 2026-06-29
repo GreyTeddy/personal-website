477ece49608e59e8a234f51e1cd3b4989b0eca85
Dennis Duka <dukadennis7@gmail.com>
2026-06-29 23:27:39 +0100

    move keyframes outside component and fix hooks violation in Background

--- components/background.tsx
+++ components/background.tsx
@@ -1,32 +1,31 @@
 import { Box, keyframes, useColorModeValue } from "@chakra-ui/react"
 
+const image = 'url("../images/teddy_icon_vertices.svg")';
+
+const rotationKeyframe = keyframes`
+    0% {
+      transform: translate3d(0,-5px, 0);
+    }
+    50% {
+      transform: translate3d(0, 5px, 0);
+    }
+    100% {
+      transform: translate3d(0,-5px, 0);
+    }
+  `
+
 const Background = () => {
-
-  const darkImage = 'url("../images/teddy_icon_vertices.svg")';
-  const lightImage = 'url("../images/teddy_icon_vertices.svg")';
-
-  const rotationKeyframe = keyframes`
-      0% {
-        transform: translate3d(0,-5px, 0);
-      }
-      50% {
-        transform: translate3d(0, 5px, 0);
-      }
-      100% {
-        transform: translate3d(0,-5px, 0);
-      }
-    `
+  const bgFilter = useColorModeValue('invert(82%)', 'invert(40%)')
 
   return (
     <Box sx={{
       zIndex: -1,
       position: 'fixed',
-      // bottom: 0,
       left: "50%",
       height: "550px",
       width: "730px",
-      background: useColorModeValue(lightImage, darkImage),
-      filter: useColorModeValue('invert(82%)','invert(40%)'),
+      background: image,
+      filter: bgFilter,
       animation: `${rotationKeyframe} 10s ease-in-out infinite`
     }}></Box>
   )
