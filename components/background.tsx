a22d64066c6a155edfb712cf0453b85b8a376801
Dennis Duka <dukadennis7@gmail.com>
2026-06-30 00:34:01 +0100

    smoother pop up of background teddy

--- components/background.tsx
+++ components/background.tsx
@@ -1,7 +1,16 @@
-import { Box, useColorModeValue } from "@chakra-ui/react"
-import { keyframes } from "@emotion/react"
+import { Box, useColorModeValue } from '@chakra-ui/react';
+import { keyframes } from '@emotion/react';
 
 const image = 'url("../images/teddy_icon_vertices.svg")';
+
+const fadeInKeyframe = keyframes`
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  `;
 
 const rotationKeyframe = keyframes`
     0% {
@@ -13,24 +22,26 @@ const rotationKeyframe = keyframes`
     100% {
       transform: translate3d(0,-5px, 0);
     }
-  `
+  `;
 
 const Background = () => {
-  const bgFilter = useColorModeValue('invert(82%)', 'invert(40%)')
+  const bgFilter = useColorModeValue('invert(82%)', 'invert(40%)');
 
   return (
-    <Box sx={{
-      zIndex: -1,
-      position: 'fixed',
-      left: "50%",
-      height: "550px",
-      width: "730px",
-      background: image,
-      filter: bgFilter,
-      willChange: 'transform',
-      animation: `${rotationKeyframe} 10s ease-in-out infinite`
-    }}></Box>
-  )
-}
+    <Box
+      sx={{
+        zIndex: -1,
+        position: 'fixed',
+        left: '50%',
+        height: '550px',
+        width: '730px',
+        background: image,
+        filter: bgFilter,
+        willChange: 'transform',
+        animation: `${fadeInKeyframe} 1s ease-in-out forwards, ${rotationKeyframe} 10s ease-in-out infinite`,
+      }}
+    ></Box>
+  );
+};
 
-export default Background
+export default Background;
